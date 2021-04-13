d94210ab5a7bc76991707239667e3f31da2c7201
akarakin <bulik889@gmail.com>
2021-04-13 18:28:39 +0300

    edited

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,8 +1,22 @@
-<!-- resources/views/about.blade.php -->
+<!-- Хранится в resources/views/about.blade.php -->
 
-<h1>О блоге</h1>
-<p>Эксперименты с Laravel на Хекслете </p>
+@extends('layouts.app')
+
+<!-- Секция, содержимое которой обычный текст. -->
+@section('title', 'О блоге')
+
+<!-- Секция, содержащая HTML блок. Имеет открывающую и закрывающую часть. -->
+@section('content')
+    <h1>О блоге</h1>
+    <p>Эксперименты с Laravel на Хекслете</p>
+@endsection
+
 <p>{{ implode(', ', $tags) }}</p>
+
+
+
+
+
 
 
 {{-- Это общие директивы для шаблонизатора blade --}}
