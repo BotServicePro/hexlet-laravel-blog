3c49fe463f39094d784acbf0bee665c6a33b2079
akarakin <bulik889@gmail.com>
2021-04-14 12:13:44 +0300

    Maket edited

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,15 +5,16 @@
 <!-- Секция, содержимое которой обычный текст. -->
 @section('title', 'О блоге')
 
+@section('header')
+    <h1>Хедер</h1>
+    <p>Пару ссылок</p>
+@endsection
+
 <!-- Секция, содержащая HTML блок. Имеет открывающую и закрывающую часть. -->
 @section('content')
     <h1>О блоге</h1>
-    <p>Эксперименты с Laravel на Хекслете</p>
+    <p>Эксперименты с Laravel на Хекслете!</p>
 @endsection
-
-<p>{{ implode(', ', $tags) }}</p>
-
-
 
 
 
@@ -27,3 +28,32 @@
 {{-- @foreach ($users as $user) --}}
 {{--    <p>This is user {{ $user->id }}</p> --}}
 {{--    @endforeach --}}
+
+{{-- @if (count($records) === 1) --}}
+{{--     I have one record! --}}
+{{-- @elseif (count($records) > 1) --}}
+{{--     I have multiple records! --}}
+{{-- @else --}}
+{{--     I don't have any records! --}}
+{{-- @endif --}}
+
+{{-- @isset($records) --}}
+{{--     // Переменная $records определена и не равна `null` ... --}}
+{{-- @endisset --}}
+
+{{-- @empty($records) --}}
+{{--     // Переменная $records считается «пустой» ... --}}
+{{-- @endempty --}}
+
+{{-- @switch($i) --}}
+{{--     @case(1) --}}
+{{--     First case... --}}
+{{--     @break --}}
+
+{{--     @case(2) --}}
+{{--     Second case... --}}
+{{--     @break --}}
+
+{{--     @default --}}
+{{--     Default case... --}}
+{{-- @endswitch --}}
